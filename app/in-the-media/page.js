e4aad3d81701faa1787838abf8f0953287d7fa1c
kallolghosal <kghosal@gmail.com>
2024-01-04 18:01:20 +0530

    Media page update

--- app/in-the-media/page.js
+++ app/in-the-media/page.js
@@ -237,6 +237,96 @@ const InTheMedia = () => {
                     </div>
                 </Col>
             </Row>
+            <br />
+            <Row>
+                <Col md={4}>
+                    <div className="medialinkbox" style={{paddingTop:'120px'}}>
+                        <Link href={'https://www.businesstoday.in/pti-feed/story/changemakers-from-public-private-and-development-sector-to-underscore-long-term-support-for-strengthening-social-innovation-and-scaling-up-impact-223580-2019-08-30'} target="_blank">
+                            <Image src={'/img/banner-img-16.png'} width={120} height={120} style={{width:'80%',height:'auto',marginLeft:'40px'}} alt="Media 1" />
+                        </Link>
+                        <div className="txtbox">
+                            <Link href={'https://www.businesstoday.in/pti-feed/story/changemakers-from-public-private-and-development-sector-to-underscore-long-term-support-for-strengthening-social-innovation-and-scaling-up-impact-223580-2019-08-30'} target="_blank">
+                            Changemakers From Public, Private and Development Sector to Underscore Long-Term Support for Strengthening Social Innovation and Scaling up Impact
+                            </Link>
+                            <br />
+                            <p>2022-07-05<br /><Link href={'https://www.businesstoday.in/pti-feed/story/changemakers-from-public-private-and-development-sector-to-underscore-long-term-support-for-strengthening-social-innovation-and-scaling-up-impact-223580-2019-08-30'} target="_blank">Published by Business Today</Link></p>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={4}>
+                    <div className="medialinkbox">
+                        <Link href={'https://yourstory.com/socialstory/2021/02/covid-action-collab-helping-pandemic-times-catalyst-group'} target="_blank">
+                            <Image src={'/img/banner-img-17.jpeg'} width={120} height={120} style={{width:'100%',height:'auto'}} alt="Media 1" />
+                        </Link>
+                        <div className="txtbox">
+                            <Link href={'https://yourstory.com/socialstory/2021/02/covid-action-collab-helping-pandemic-times-catalyst-group'} target="_blank">
+                            This non-for-profit collaborative has delivered over 1.7 lakh services amidst the COVID-19 pandemic
+                            </Link>
+                            <br />
+                            <p>2022-07-05<br /><Link href={'https://yourstory.com/socialstory/2021/02/covid-action-collab-helping-pandemic-times-catalyst-group'} target="_blank">Published by Social Story</Link></p>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={4}>
+                <div className="medialinkbox" style={{paddingTop:'120px'}}>
+                        <Link href={'https://www.theweek.in/wire-updates/national/2019/08/29/nrg31-dl-forum-solutions%20(r).html'} target="_blank">
+                            <Image src={'/img/banner-img-18.png'} width={120} height={120} style={{width:'80%',height:'auto', marginLeft:'30px'}} alt="Media 1" />
+                        </Link>
+                        <div className="txtbox">
+                            <Link href={'https://www.theweek.in/wire-updates/national/2019/08/29/nrg31-dl-forum-solutions%20(r).html'} target="_blank">
+                            Multi-stakeholder forum hosted in Bengaluru for bringing innovative solutions
+                            </Link>
+                            <br />
+                            <p>2022-07-05<br /><Link href={'https://www.theweek.in/wire-updates/national/2019/08/29/nrg31-dl-forum-solutions%20(r).html'} target="_blank">Published by The Week</Link></p>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+            <br />
+            <Row>
+                <Col md={4}>
+                    <div className="medialinkbox">
+                        <Link href={'https://www.businessworld.in/article/Over-150-Organizations-Come-Together-To-Form-COVIDActionCollab/22-04-2020-189942/'} target="_blank">
+                            <Image src={'/img/banner-img-19.jpeg'} width={120} height={120} style={{width:'100%',height:'auto'}} alt="Media 1" />
+                        </Link>
+                        <div className="txtbox">
+                            <Link href={'https://www.businessworld.in/article/Over-150-Organizations-Come-Together-To-Form-COVIDActionCollab/22-04-2020-189942/'} target="_blank">
+                            Over 150 Organizations Come Together To Form #COVIDActionCollab
+                            </Link>
+                            <br />
+                            <p>2022-07-05<br /><Link href={'https://www.businessworld.in/article/Over-150-Organizations-Come-Together-To-Form-COVIDActionCollab/22-04-2020-189942/'} target="_blank">Published by BusinessLine</Link></p>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={4}>
+                    <div className="medialinkbox">
+                        <Link href={'https://everythingexperiential.businessworld.in/article/Do-social-impact-organizations-adopt-technology-successfully-by-Aveek-De-and-Neelima-Kurup/19-08-2019-174935/'} target="_blank">
+                            <Image src={'/img/banner-img-20.jpeg'} width={120} height={120} style={{width:'100%',height:'auto'}} alt="Media 1" />
+                        </Link>
+                        <div className="txtbox">
+                            <Link href={'https://everythingexperiential.businessworld.in/article/Do-social-impact-organizations-adopt-technology-successfully-by-Aveek-De-and-Neelima-Kurup/19-08-2019-174935/'} target="_blank">
+                            Do social impact organizations adopt technology successfully: Aveek De and Neelima Kurup
+                            </Link>
+                            <br />
+                            <p>2022-07-05<br /><Link href={'https://everythingexperiential.businessworld.in/article/Do-social-impact-organizations-adopt-technology-successfully-by-Aveek-De-and-Neelima-Kurup/19-08-2019-174935/'} target="_blank">Published by Business World</Link></p>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={4}>
+                <div className="medialinkbox">
+                        <Link href={'https://www.financialexpress.com/lifestyle/changing-behaviors-changing-lives-three-major-problems-that-can-save-millions-of-lives/1689608/'} target="_blank">
+                            <Image src={'/img/banner-img-21.jpeg'} width={120} height={120} style={{width:'100%',height:'auto'}} alt="Media 1" />
+                        </Link>
+                        <div className="txtbox">
+                            <Link href={'https://www.financialexpress.com/lifestyle/changing-behaviors-changing-lives-three-major-problems-that-can-save-millions-of-lives/1689608/'} target="_blank">
+                            Changing Behaviors, Changing Lives: Three major problems that can save millions of live
+                            </Link>
+                            <br />
+                            <p>2022-07-05<br /><Link href={'https://www.financialexpress.com/lifestyle/changing-behaviors-changing-lives-three-major-problems-that-can-save-millions-of-lives/1689608/'} target="_blank">Published by Financial Express</Link></p>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
         </Container>
         <Footer />
         <Copyright />
